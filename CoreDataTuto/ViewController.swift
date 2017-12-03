1e7079e33e80b7a20054cf5666304490045104de
imrabti <imrabti@gmail.com>
2017-12-03 11:35:11 +0100

    Initial Commit

---
+++ CoreDataTuto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTuto
+//
+//  Created by Mrabti Idriss on 03.12.17.
+//  Copyright © 2017 Mrabti Idriss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
