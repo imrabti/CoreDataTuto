4346a35483bc50de21b99f14ba88c8819e286f51
Mrabti <imrabti@gmail.com>
2017-12-09 22:46:49 +0100

    Initial Draft integration of Sections.

--- CoreDataTuto/ContactsTableViewController.swift
+++ CoreDataTuto/ContactsTableViewController.swift
@@ -11,7 +11,9 @@ import CoreData
 import SwiftRecord
 
 class ContactsTableViewController: UITableViewController {
-    var contacts: [Contact] = []
+    var contacts: [[Contact]] = []
+    var contactSections: [String] = []
+    
     var filteredContacts: [Contact] = []
     var selectedRowToEdit: IndexPath?
     
@@ -38,16 +40,43 @@ class ContactsTableViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        contacts = Contact.all() as! [Contact]
+        // Load all contacts
+        let allContacts = Contact.all() as! [Contact]
+        
+        // Group contacts by section
+        for contactType in ContactType.allValues {
+            let dataForSection = allContacts.filter({ $0.type == contactType.rawValue })
+            
+            if dataForSection.count > 0 {
+                contacts.append(dataForSection)
+                contactSections.append(contactType.rawValue)
+            }
+        }
     }
     
     // MARK: - Table view data source
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if tableView == self.tableView {
+            return contactSections.count
+        } else {
+            return 1
+        }
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if tableView == self.tableView {
-            return self.contacts.count
+            return contacts[section].count
         } else {
             return self.filteredContacts.count
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if (tableView == self.tableView) {
+            return contactSections[section]
+        } else {
+            return nil
         }
     }
     
@@ -56,17 +85,17 @@ class ContactsTableViewController: UITableViewController {
             fatalError("The dequeued cell is not an instance of ContactTableViewCell.")
         }
         
-        cell.fullName.text = tableView == self.tableView ? contacts[indexPath.row].name : filteredContacts[indexPath.row].name
-        cell.email.text = tableView == self.tableView ? contacts[indexPath.row].email : filteredContacts[indexPath.row].email
-        cell.phoneNumber.text = tableView == self.tableView ? contacts[indexPath.row].phoneNumber : filteredContacts[indexPath.row].phoneNumber
+        cell.fullName.text = tableView == self.tableView ? contacts[indexPath.section][indexPath.row].name : filteredContacts[indexPath.row].name
+        cell.email.text = tableView == self.tableView ? contacts[indexPath.section][indexPath.row].email : filteredContacts[indexPath.row].email
+        cell.phoneNumber.text = tableView == self.tableView ? contacts[indexPath.section][indexPath.row].phoneNumber : filteredContacts[indexPath.row].phoneNumber
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         let delete = UITableViewRowAction(style: .normal, title: "Delete") { action, index in
-            self.contacts[index.row].delete()
-            self.contacts.remove(at: index.row)
+            self.contacts[indexPath.section][indexPath.row].delete()
+            self.contacts[indexPath.section].remove(at: index.row)
             
             tableView.reloadData()
         }
@@ -74,7 +103,7 @@ class ContactsTableViewController: UITableViewController {
         
         let edit = UITableViewRowAction(style: .normal, title: "Edit") { action, index in
             self.selectedRowToEdit = index
-            self.performSegue(withIdentifier: "EditContact", sender: self.contacts[index.row])
+            self.performSegue(withIdentifier: "EditContact", sender: self.contacts[index.section][index.row])
         }
         
         return [edit, delete]
@@ -99,16 +128,24 @@ class ContactsTableViewController: UITableViewController {
         if let sourceViewController = sender.source as? EditContactViewController, let contact = sourceViewController.addOrEditContact {
             if let selectedIndexPath = selectedRowToEdit {
                 // Updated model with new contact
-                contacts[selectedIndexPath.row] = contact
+                contacts[selectedIndexPath.section][selectedIndexPath.row] = contact
                 selectedRowToEdit = nil
+                
+                // TODO : Change from section to section when the ContactType is modified ...
                 
                 tableView.reloadData()
             } else {
-                // Add a new contact.
-                let newIndexPath = IndexPath(row: contacts.count, section: 0)
-                
-                contacts.append(contact)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
+                if let section = contactSections.index(where: { $0 == contact.type }) {
+                    let newIndexPath = IndexPath(row: contacts[section].count, section: section)
+                    
+                    contacts[section].append(contact)
+                    tableView.insertRows(at: [newIndexPath], with: .automatic)
+                } else {
+                    let newIndexSet = IndexSet()
+                    
+                    contactSections.append(contact.type!)
+                    tableView.insertSections(newIndexSet, with: .automatic)
+                }
             }
         }
     }
@@ -123,12 +160,12 @@ extension ContactsTableViewController: UISearchBarDelegate {
 
 extension ContactsTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        self.filteredContacts = self.contacts.filter { (contact: Contact) -> Bool in
+        /*self.filteredContacts = self.contacts.filter { (contact: Contact) -> Bool in
             if let name = contact.name {
                 return name.lowercased().contains(self.searchController.searchBar.text!.lowercased())
             }
             return false
-        }
+        }*/
         
         self.resultsController.tableView.reloadData()
     }
